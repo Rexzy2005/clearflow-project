9d8020b56c74ebdf8bc867482bc024724fb09057
DevRex <timothypererat2004@gmail.com>
2025-08-28 00:42:29 +0100

    Initial commit on workspace branch

--- scripts.js
+++ scripts.js
@@ -1,4 +1,18 @@
 const faqCards = document.querySelectorAll('.faq-card');
+const codes = document.querySelectorAll('.code');
+
+codes[0].focus()
+
+codes.forEach((code, index) => {
+  code.addEventListener('keydown', (e) => {
+    if(e.key >= 0 && e.key <= 9){
+      codes[index].value = ''
+      setTimeout(() => codes[index + 1].focus(), 10);
+    } else if(e.key === "Backspace"){
+      setTimeout(() => codes[index - 1].focus(), 10)
+    }
+  })
+});
 
 faqCards.forEach(card => {
   const question = card.querySelector('.faq-question');
